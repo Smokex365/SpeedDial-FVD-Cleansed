c02ed8c8d72bd38cb4cdf177bb18b45eba24ec53
Smokex365 <smokex365@gmail.com>
2021-09-26 19:01:47 -0400

    space/indent cleanup; commented out ad check

--- js/config.js
+++ js/config.js
@@ -3,7 +3,8 @@ fvdSpeedDial.Config = {
   ANALYTICS_APP: 'FVDSD Chrome',
   FANCY_SIDE_DIALS_MAX_SCALE: 5,
 	FANCY_SIDE_DIALS_MIN_SCALE: 1.3,
-	DISPLAY_AD_EVERY: (Math.floor(Math.random() * 3) + 3) * 24 * 3600 * 1000, // 3 - 5 days
+	DISPLAY_AD_EVERY: 0, // Don't display ad at all quick fix
+  //DISPLAY_AD_EVERY: (Math.floor(Math.random() * 3) + 3) * 24 * 3600 * 1000, // 3 - 5 days
   FS_DIALS_PREVIEW_DIR: "sd_previews",
   FS_MOSTVISITED_PREVIEW_DIR: "mostvisited_previews",
   FS_MISC_DIR: "misc",
